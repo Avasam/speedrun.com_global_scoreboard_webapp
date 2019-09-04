28ef53f062fa6f81ba7bb0aa2b20a992aa55c8f1
Avasam <samuel.06@hotmail.com>
2019-09-03 23:22:33 -0400

    Header/Footer finished. Login Modal WIP

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ScoreboardNavBar from './NavBar/ScoreboardNavBar';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <ScoreboardNavBar username={null}></ScoreboardNavBar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +23,9 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <footer>
+        &copy; <a href="https://github.com/Avasam/speedrun.com_global_leaderboard_webapp/blob/master/LICENSE" target="about">Copyright</a> {new Date().getFullYear()} by <a href="https://github.com/Avasam/" target="about">Samuel Therrien</a>.
+        Powered by <a href="https://www.speedrun.com/" target="src">speedrun.com</a> and <a href="https://www.pythonanywhere.com/" target="about">PythonAnywhere</a></footer>
     </div>
   );
 }
