ec48acc9372fb2398cc5c886e13a80e5a00120f9
Avasam <samuel.06@hotmail.com>
2020-04-10 22:35:38 -0400

    Improved some user messages

--- tournament-scheduler/src/ScheduleRegistration/ScheduleRegistration.tsx
+++ tournament-scheduler/src/ScheduleRegistration/ScheduleRegistration.tsx
@@ -23,7 +23,7 @@ const postRegistration = (timeSlotId: number, participants: string[], registrati
     .then(res => res.json())
 
 const ScheduleRegistration: FC<ScheduleRegistrationProps> = (props: ScheduleRegistrationProps) => {
-  const [schedule, setSchedule] = useState<Schedule | undefined | null>(undefined)
+  const [schedule, setSchedule] = useState<Schedule | null | undefined>()
   const [registrationKey, setRegistrationKey] = useState<string>('')
   const [selectedTimeSlot, setSelectedTimeSlot] = useState<TimeSlot | undefined>()
   const [timeSlotLabelWidth, setTimeSlotLabelWidth] = useState(0)
@@ -106,95 +106,93 @@ const ScheduleRegistration: FC<ScheduleRegistrationProps> = (props: ScheduleRegi
   }
 
   return <Container>
-    {schedule !== undefined && (!schedule
-      ? <div>Sorry. `<code>{props.registrationLink}</code>` does not lead to an existing registration form.</div>
-      : <>
-        <span style={{ display: 'block' }}>All dates and times are given in your local timezone.</span>
-        <Card>
-          <CardContent style={{ textAlign: 'left' }}>
-            <label>Schedule for: {schedule.name}</label>
-            <Link href={`${window.location.href}?view=${schedule.id}`} target="blank" style={{ display: 'block' }}>
-              Click here to view the current registrations in a new tab
-            </Link>
-            {!schedule.active
-              ? <div><br />Sorry. This schedule is currently inactive and registration is closed.</div>
-              : <FormGroup>
-                <FormControl variant="outlined" style={{ margin: '16px 0' }}>
-                  <InputLabel
-                    ref={timeSlotInputLabel}
-                    id="time-slot-select-label"
-                    style={{ paddingRight: `${timeSlotLabelPaddingRight}px` }}
-                  >
-                    Choose your time slot amongst the following
-                  </InputLabel>
-                  <Select
-                    labelId="time-slot-select-label"
-                    id="time-slot-select"
-                    value={selectedTimeSlot?.id || ''}
-                    onChange={selectTimeSlot}
-                    labelWidth={timeSlotLabelWidth}
-                  >
-                    {schedule.timeSlots.map(timeSlot =>
-                      <MenuItem
-                        key={`timeslot-${timeSlot.id}`}
-                        value={timeSlot.id}
-                        disabled={entriesLeft(timeSlot) <= 0 || timeSlot.dateTime <= new Date()}
-                      >{
-                          moment(timeSlot.dateTime).format(`ddd ${new DateFnsUtils().dateTime24hFormat}`) +
-                          ' (' +
-                          (timeSlot.dateTime <= new Date()
-                            ? 'past deadline'
-                            : entriesLeft(timeSlot) <= 0
-                              ? 'full'
-                              : `${entriesLeft(timeSlot)} / ${timeSlot.maximumEntries}` +
-                              ` entr${entriesLeft(timeSlot) === 1 ? 'y' : 'ies'} left`) +
-                          ')'
-                        }</MenuItem>
-                    )}
-                  </Select>
-                </FormControl>
-                {selectedTimeSlot && entriesLeft(selectedTimeSlot) > 0 &&
-                  <>
-                    <FormLabel>
-                      Please write down your name
-                      {selectedTimeSlot.participantsPerEntry > 1 &&
-                        ' as well as all other participants playing with or against you in the same match'}
-                    </FormLabel>
-                    {(Array(...Array(selectedTimeSlot.participantsPerEntry))).map((_, index) =>
-                      <TextField
-                        key={`participant-${index}`}
-                        label={`Participant${selectedTimeSlot.participantsPerEntry > 1 ? ` ${index + 1}` : ''}'s name`}
-                        onChange={event => handleParticipantChange(index, event.target.value)}
-                      />
-                    )}
-                  </>}
-              </FormGroup>
+    {!schedule
+      ? schedule === null && <div>Sorry. `<code>{props.registrationLink}</code>` does not lead to an existing registration form.</div>
+      : <Card>
+        <CardContent style={{ textAlign: 'left' }}>
+          <label>Schedule for: {schedule.name}</label>
+          <Link href={`${window.location.href}?view=${schedule.id}`} target="blank" style={{ display: 'block' }}>
+            Click here to view the current registrations in a new tab
+          </Link>
+          <span style={{ display: 'block' }}>All dates and times are given in your local timezone.</span>
+          {!schedule.active
+            ? <div><br />Sorry. This schedule is currently inactive and registration is closed.</div>
+            : <FormGroup>
+              <FormControl variant="outlined" style={{ margin: '16px 0' }}>
+                <InputLabel
+                  ref={timeSlotInputLabel}
+                  id="time-slot-select-label"
+                  style={{ paddingRight: `${timeSlotLabelPaddingRight}px` }}
+                >
+                  Choose your time slot amongst the following
+                </InputLabel>
+                <Select
+                  labelId="time-slot-select-label"
+                  id="time-slot-select"
+                  value={selectedTimeSlot?.id || ''}
+                  onChange={selectTimeSlot}
+                  labelWidth={timeSlotLabelWidth}
+                >
+                  {schedule.timeSlots.map(timeSlot =>
+                    <MenuItem
+                      key={`timeslot-${timeSlot.id}`}
+                      value={timeSlot.id}
+                      disabled={entriesLeft(timeSlot) <= 0 || timeSlot.dateTime <= new Date()}
+                    >{
+                        moment(timeSlot.dateTime).format(`ddd ${new DateFnsUtils().dateTime24hFormat}`) +
+                        ' (' +
+                        (timeSlot.dateTime <= new Date()
+                          ? 'past deadline'
+                          : entriesLeft(timeSlot) <= 0
+                            ? 'full'
+                            : `${entriesLeft(timeSlot)} / ${timeSlot.maximumEntries}` +
+                            ` entr${entriesLeft(timeSlot) === 1 ? 'y' : 'ies'} left`) +
+                        ')'
+                      }</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+              {selectedTimeSlot && entriesLeft(selectedTimeSlot) > 0 &&
+                <>
+                  <FormLabel>
+                    Please write down your name
+                    {selectedTimeSlot.participantsPerEntry > 1 &&
+                      ' as well as all other participants playing with or against you in the same match'}
+                  </FormLabel>
+                  {(Array(...Array(selectedTimeSlot.participantsPerEntry))).map((_, index) =>
+                    <TextField
+                      key={`participant-${index}`}
+                      label={`Participant${selectedTimeSlot.participantsPerEntry > 1 ? ` ${index + 1}` : ''}'s name`}
+                      onChange={event => handleParticipantChange(index, event.target.value)}
+                    />
+                  )}
+                </>}
+            </FormGroup>
+          }
+        </CardContent>
+        <CardActions>
+          <Button
+            size='small'
+            variant='contained'
+            color='primary'
+            disabled={
+              !formValidity ||
+              !selectedTimeSlot ||
+              entriesLeft(selectedTimeSlot) <= 0
             }
-          </CardContent>
-          <CardActions>
-            <Button
-              size='small'
-              variant='contained'
-              color='primary'
-              disabled={
-                !formValidity ||
-                !selectedTimeSlot ||
-                entriesLeft(selectedTimeSlot) <= 0
-              }
-              onClick={() =>
-                formValidity &&
-                selectedTimeSlot &&
-                entriesLeft(selectedTimeSlot) > 0 &&
-                sendRegistrationForm()
-              }
-            >
-              Sign {selectedTimeSlot?.participantsPerEntry === 1 ? 'me' : 'us'} up!
-            </Button>
-            <span style={{ color: 'red' }}>{errorMessage}</span>
-          </CardActions>
-        </Card>
-      </>
-    )}
+            onClick={() =>
+              formValidity &&
+              selectedTimeSlot &&
+              entriesLeft(selectedTimeSlot) > 0 &&
+              sendRegistrationForm()
+            }
+          >
+            Sign {selectedTimeSlot?.participantsPerEntry === 1 ? 'me' : 'us'} up!
+          </Button>
+          <span style={{ color: 'red' }}>{errorMessage}</span>
+        </CardActions>
+      </Card>
+    }
 
   </Container>
 }
