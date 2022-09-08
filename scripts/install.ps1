035fd15d629211cbd35661ea3c86b17098fe38ee
Avasam <samuel.06@hotmail.com>
2022-09-08 15:30:21 -0400

    Updated configs and autofixers

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,4 +1,9 @@
-py -3.9 -m pip install wheel --upgrade
-py -3.9 -m pip install -r "$PSScriptRoot/requirements.txt"
-npm install -g pyright@latest
-npm list -g pyright
+# Alias python3 to python on Windows
+If ($IsWindows) {
+  $python = (Get-Command python).Source
+  $python3 = "$((Get-Item $python).Directory.FullName)/python3.exe"
+  New-Item -ItemType SymbolicLink -Path $python3 -Target $python -ErrorAction SilentlyContinue
+}
+
+python3 -m pip install wheel pip setuptools --upgrade
+python3 -m pip install -r "$PSScriptRoot/requirements.txt"
